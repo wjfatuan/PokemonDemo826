5e8a638fc31e7c31e82f966878a19d736f8d62fd
wjforero <1499698+wjforero@users.noreply.github.com>
2025-08-28 20:54:39 -0500

    Using ListView

--- app/src/main/java/co/edu/uan/pokemondemo826/MainActivity.kt
+++ app/src/main/java/co/edu/uan/pokemondemo826/MainActivity.kt
@@ -1,6 +1,8 @@
 package co.edu.uan.pokemondemo826
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat
 import co.edu.uan.pokemondemo826.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
@@ -20,11 +24,20 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.rbChamsey.setOnClickListener {
-            binding.ivPokemon.setImageResource(R.drawable.maggie_simpson)
+            binding.ivPokemon.setImageResource(R.drawable.chamsey)
         }
         binding.rbPikachu.setOnClickListener {
-            binding.ivPokemon.setImageResource(R.drawable.bart_simpson_200px)
-
+            binding.ivPokemon.setImageResource(R.drawable.pikachu)
+        }
+        binding.lvPokemon.setOnItemClickListener { parent, view, position, id ->
+            println("parent: $parent, view: $view, position: $position, id: $id")
+            val textView = view as TextView
+            if(textView.text.toString().lowercase() == "pikachu") {
+                binding.ivPokemon.setImageResource(R.drawable.pikachu)
+            }
+            else if (textView.text.toString().lowercase() == "chamsey") {
+                binding.ivPokemon.setImageResource(R.drawable.chamsey)
+            }
         }
     }
 }
